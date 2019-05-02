e9d0f29daf4eb062167057b4cff5d3f6a591148a
LIParadise <a58524andy@gmail.com>
2019-05-02 11:18:46 +0800

    not initial, but I've do nothing

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./00_clean_all.sh;./01_run_HCopy.sh;./02_run_HCompV.sh;./03_training.sh;./04_testing.sh
---
+++ set_htk_path.sh
@@ -0,0 +1,1 @@
+PATH=$PATH:"/mnt/data/Document/DSP/dsp_hw2_v1/htk341_debian_x86_64"
